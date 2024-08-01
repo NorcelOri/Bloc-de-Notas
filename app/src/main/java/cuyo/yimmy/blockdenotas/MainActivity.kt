433732d2ca761984ceae4eb2c8439f8a01d7fa97
NorcelOri <yimmyyeysoncuyozamata@gmail.com>
2024-07-31 23:57:07 -0500

    sexto

--- app/src/main/java/cuyo/yimmy/blockdenotas/MainActivity.kt
+++ app/src/main/java/cuyo/yimmy/blockdenotas/MainActivity.kt
@@ -12,14 +12,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.PopupWindow
 import androidx.core.content.ContextCompat
-
+import android.util.TypedValue
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textoEditable:  EditText
+    private lateinit var textoEditable: EditText
     private lateinit var contenedor: LinearLayout
     private lateinit var imagenMenu: ImageView
+    private lateinit var imagenOpciones: ImageView
     private lateinit var llTodasLasNotas: LinearLayout
     private lateinit var llPapelera: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.introducion)
@@ -27,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         textoEditable = findViewById(R.id.textoEditable)
         contenedor = findViewById(R.id.contenedor)
         imagenMenu = findViewById(R.id.imagenMenu)
+        imagenOpciones = findViewById(R.id.imagenOpciones)
 
         textoEditable.setOnEditorActionListener { _, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_DONE ||
@@ -37,21 +40,22 @@ class MainActivity : AppCompatActivity() {
             } else {
                 false
             }
-
         }
-        imagenMenu.setOnClickListener{ verVentana(it)}
+
+        imagenMenu.setOnClickListener { verVentanaEmergente1(it) }
+        imagenOpciones.setOnClickListener { verVentanaEmergente2(it) }
     }
 
-    private fun addTextView(text:String){
+    private fun addTextView(text: String) {
         val textView = TextView(this)
         textView.text = text
         val layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT,)
-
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
         layoutParams.setMargins(16, 16, 16, 16)
 
-        textView.setLayoutParams(layoutParams)
+        textView.layoutParams = layoutParams
         textView.setBackgroundResource(R.drawable.border_completo)
         textView.setBackgroundColor(android.graphics.Color.BLACK)
         textView.setTextColor(android.graphics.Color.WHITE)
@@ -60,14 +64,32 @@ class MainActivity : AppCompatActivity() {
         contenedor.addView(textView)
     }
 
-    private fun verVentana(anchor: View) {
+    private fun verVentanaEmergente1(anchor: View) {
         val inflater: LayoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val popupView = inflater.inflate(R.layout.ventana_emergente_2, null)
-
+        val popupView = inflater.inflate(R.layout.ventana_emergente, null)
         val popupWindow = PopupWindow(
             popupView,
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT,
+            true
+        )
+        popupWindow.showAsDropDown(anchor, -anchor.width / 2, 0)
+    }
+
+    private fun verVentanaEmergente2(anchor: View) {
+        val inflater: LayoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.ventana_emergente_2, null)
+        val heightInDp = 550
+        val heightInPx = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            heightInDp.toFloat(),
+            resources.displayMetrics
+        ).toInt()
+
+        val popupWindow = PopupWindow(
+            popupView,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            heightInPx,
             true
         )
         popupWindow.showAsDropDown(anchor, -anchor.width / 2, 0)
@@ -84,7 +106,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun selectOption(selected: LinearLayout, unselected: LinearLayout, selectedTextId: Int, unselectedTextId: Int) {
-        selected.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+        selected.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
         selected.findViewById<TextView>(selectedTextId).setTextColor(ContextCompat.getColor(this, R.color.white))
         unselected.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
         unselected.findViewById<TextView>(unselectedTextId).setTextColor(ContextCompat.getColor(this, R.color.black))
